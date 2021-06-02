97508296866d460d70b2e073b4ed06e05e463916
Nate Sigmon <sigmonate@gmail.com>
2021-06-01 20:40:46 -0700

    fixed the issue preventing the graphs from showing

--- finalprojq4-nate/server.R
+++ finalprojq4-nate/server.R
@@ -8,6 +8,8 @@
 #
 
 library(shiny)
+library(tidyverse)
+library(ggplot2)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -15,7 +17,7 @@ shinyServer(function(input, output) {
     output$distPlot <- renderPlot({
 
         
-        data <- read.csv('GAS-DATA-AS-CSV.csv')
+        data <- read.csv('GAS-DATA-AS-CSV.csv.bz2')
         data2 <- data %>% 
             pivot_longer(
                 contains("x"),
@@ -27,12 +29,38 @@ shinyServer(function(input, output) {
         data3 <- data2 %>% 
             pivot_wider(date,
                         names_from = "City",
-                        values_from = "price") %>% 
-            group_by(date)
+                        values_from = "price") %>%
+            select(-date)
         
+        #data3$Seattle <- unlist(data3$Seattle)
+        #data3$Boston <- unlist(data3$Boston)
+        #data3$Chicago <- unlist(data3$Chicago)
+        #data3$Denver <- unlist(data3$Denver)
+        #data3$Cleveland <- unlist(data3$Cleveland)
+        #data3$Houston <- unlist(data3$Houston)
+        #data3$NewYork <- unlist(data3$NewYork)
+        #data3$LosAngeles <- unlist(data3$LosAngeles)
+        #data3$SanFrancisco <- unlist(data3$SanFrancisco)
+        #data3$Miami <- unlist(data3$Miami)
+        
+        #as.integer(data3$Seattle)
+        #as.integer(data3$Boston)
+        #as.integer(data3$Chicago)
+        #as.integer(data3$Cleveland)
+        #as.integer(data3$Denver)
+        #as.integer(data3$LosAngeles)
+        #as.integer(data3$Miami)
+        #as.integer(data3$NewYork)
+        #as.integer(data3$Houston)
+        #as.integer(data3$SanFrancisco)
+        
+        
+        x <- input$Cityone
+        y<- input$Citytwo
         
         ggplot(data3)+
-            geom_point(aes(x = input$City1, y = input$City2), col = "blue", na.rm = TRUE)
+            geom_point(aes_string(x, y), col = "blue")
+       
 
     }
 
--- finalprojq4-nate/ui.R
+++ finalprojq4-nate/ui.R
@@ -8,33 +8,33 @@
 #
 
 library(shiny)
-library(tidyverse)
-library(ggplot2)
+
 
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Gas price fluctuations"),
+    titlePanel("City vs City price relationship"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            selectInput("City1", label = ("City One"),
-                            choices = list("Boston" = "Boston", "Chicago" = "Chicago",
-                                           "Cleveland" = "Cleveland", "Denver" = "Denver",
-                                           "Houston" = "Houston", "Los Angeles" = "LosAngeles",
-                                           "Miami" = "Miami", "New York" = "NewYork",
-                                           "San Francisco" = "SanFrancisco", "Seattle" = "Seattle"),
-                            selected = "Seattle"),
-            selectInput("City2", label = ("City Two"),
-                        choices = list("Boston" = "Boston", "Chicago" = "Chicago",
-                                       "Cleveland" = "Cleveland", "Denver" = "Denver",
-                                       "Houston" = "Houston", "Los Angeles" = "LosAngeles",
-                                       "Miami" = "Miami", "New York" = "NewYork",
-                                       "San Francisco" = "SanFrancisco", "Seattle" = "Seattle"),
-                        selected = "San Francisco")
+            selectInput("Cityone", label = h3("City One"),
+                        choices = list("Boston" = 'Boston', "Chicago" = 'Chicago',
+                                           "Cleveland" = 'Cleveland', "Denver" = 'Denver',
+                                           "Houston" = 'Houston', "Los Angeles" = 'LosAngeles',
+                                           "Miami" = 'Miami', "New York" = 'NewYork',
+                                           "San Francisco" = 'SanFrancisco', "Seattle" = 'Seattle'),
+                  selected = "Seattle"),
+            selectInput("Citytwo", label = h3("City Two"),
+                        choices = list("Boston" = 'Boston', "Chicago" = 'Chicago',
+                                       "Cleveland" = 'Cleveland', "Denver" = 'Denver',
+                                       "Houston" = 'Houston', "Los Angeles" = 'LosAngeles',
+                                       "Miami" = 'Miami', "New York" = 'NewYork',
+                                       "San Francisco" = 'SanFrancisco', "Seattle" = 'Seattle'),
+                        selected = "Boston"),
+            
         ),
 
         # Show a plot of the generated distribution
