dda903641769f77d137af98ccb7d798722365cfa
Nate Sigmon <sigmonate@gmail.com>
2021-06-01 21:49:19 -0700

    added the text output

--- finalprojq4-nate/server.R
+++ finalprojq4-nate/server.R
@@ -1,17 +1,7 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(tidyverse)
 library(ggplot2)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
     output$distPlot <- renderPlot({
@@ -31,40 +21,35 @@ shinyServer(function(input, output) {
                         names_from = "City",
                         values_from = "price") %>%
             select(-date)
-        
-        #data3$Seattle <- unlist(data3$Seattle)
-        #data3$Boston <- unlist(data3$Boston)
-        #data3$Chicago <- unlist(data3$Chicago)
-        #data3$Denver <- unlist(data3$Denver)
-        #data3$Cleveland <- unlist(data3$Cleveland)
-        #data3$Houston <- unlist(data3$Houston)
-        #data3$NewYork <- unlist(data3$NewYork)
-        #data3$LosAngeles <- unlist(data3$LosAngeles)
-        #data3$SanFrancisco <- unlist(data3$SanFrancisco)
-        #data3$Miami <- unlist(data3$Miami)
-        
-        #as.integer(data3$Seattle)
-        #as.integer(data3$Boston)
-        #as.integer(data3$Chicago)
-        #as.integer(data3$Cleveland)
-        #as.integer(data3$Denver)
-        #as.integer(data3$LosAngeles)
-        #as.integer(data3$Miami)
-        #as.integer(data3$NewYork)
-        #as.integer(data3$Houston)
-        #as.integer(data3$SanFrancisco)
-        
+
         
         x <- input$Cityone
         y<- input$Citytwo
         
-        ggplot(data3)+
-            geom_point(aes_string(x, y), col = "blue")
+        ggplot(data3,aes_string(x, y))+
+            geom_point(col = "blue")+
+            xlab(paste("Price in", input$Cityone))+
+            ylab(paste("Price in", input$Citytwo))+
+            geom_smooth(col = "red", se = FALSE)
+           
        
 
     }
+    
 
 )
+    output$textOutput <- renderText({
+        paste("This chart was designed to answer the question: 'Do the gas prices in
+        American cities fluctutate independently or together?' To answer this question,
+        the chart compares the prices of two chosen cities from the dataset. Each point
+        represents a month from June of 2000 to April of 2021. If the cities were fluctuating
+        independently, the points would be scattered across the chart.
+        Meanwhile, if the cities were fluctuating together, the points should
+        form a roughly linear trend. As you can see, every comparison between two
+        cities is roughly linear. In some cases the correlation is incredibly strong.
+        This chart thus lends support to an answer to our question: the cities
+        fluctuate together, not independently.")
+    })
 }
 )
 
--- finalprojq4-nate/ui.R
+++ finalprojq4-nate/ui.R
@@ -1,21 +1,10 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
-    # Application title
-    titlePanel("City vs City price relationship"),
+    titlePanel("City vs City Gas Price Relationship Over Time"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
@@ -39,7 +28,8 @@ shinyUI(fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+            plotOutput("distPlot"),
+            textOutput("textOutput")
         )
     )
 ))
