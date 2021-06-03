f4f6156409624e78c87b2527fd166de19f1910e4
shawk120 <shawk120@uw.edu>
2021-06-02 22:32:30 -0700

    add Leo's part

--- finalprojq4-nate/ui.R
+++ finalprojq4-nate/ui.R
@@ -26,8 +26,8 @@ shinyUI(navbarPage("Gas Prices in US Cities",
             ),
             # Show a plot of the generated distribution
             mainPanel(
-                plotOutput("distPlot"),
-                textOutput("textOutput")
+                plotOutput("firstDistPlot"),
+                textOutput("firstTextOutput")
             )
         )
     ),
@@ -43,6 +43,31 @@ shinyUI(navbarPage("Gas Prices in US Cities",
                 ),
             mainPanel(
                 tableOutput("GasData")
+            )
+        )
+    ),
+    tabPanel("Monthly Gas Prices in US Cities",
+        sidebarLayout(
+            sidebarPanel(
+                selectInput("Cityone", label = "City",
+                            choices = list("Boston" = 'Boston', "Chicago" = 'Chicago',
+                                            "Cleveland" = 'Cleveland', "Denver" = 'Denver',
+                                            "Houston" = 'Houston', "Los Angeles" = 'LosAngeles',
+                                            "Miami" = 'Miami', "New York" = 'NewYork',
+                                            "San Francisco" = 'SanFrancisco', "Seattle" = 'Seattle'),
+                            selected = "Chicago"),
+                selectInput("date", label = "Date",
+                            choices = list("2000" = '00', "2001" = '01', "2002" = '02',
+                                            "2003" = '03', "2004" = '04', "2005" = '05', "2006" = '06',
+                                            "2007" = '07', "2008" = '08', "2009" = '09', "2010" = '10', 
+                                            "2011" = '11', "2012" = '12', "2013" = '13', "2014" = '14',
+                                            "2015" = '15', "2016" = '16', "2017" = '17', "2018" = '18',
+                                            "2019" = '19', "2020" = '20', "2021" = '21'), selected = "2000")
+            ),
+            # Show a plot of the generated distribution
+            mainPanel(
+                plotOutput("secondDistPlot"),
+                textOutput("secondTextOutput")
             )
         )
     ),
