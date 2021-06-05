af29c61ba9d9ed9d959c3e43a9a445ec4d079ef9
Nate Sigmon <sigmonate@gmail.com>
2021-06-04 23:28:44 -0700

    extended the intro and conclusions

--- Final files/server.R
+++ Final files/server.R
@@ -4,6 +4,20 @@ library(ggplot2)
 
 shinyServer(function(input, output) {
     
+    
+    output$introTextOutput <- renderText({
+        paste("In this app, you will be able to review the data of average gas prices
+        in main cities in the US. The main dataset our app utilizes is from the
+        US Energy Information Administration. See the link if needed: https://www.eia.gov/dnav/pet/pet_pri_gnd_a_epm0_pte_dpgal_m.htm.
+        Our target audience is consumers of driving age located in urban areas in
+        the US. Using our app, you will be able to see how gas prices have changed
+        in urban areas in the US over time, when each city's gas prices were at
+        their highest and lowest prices, which cities have experienced the highest
+        rates of increases in gas prices, and whether cities' gas prices usually
+        fluctuate together or independently. The main libraries we used to make
+        this app were shiny, tidyverse and ggplot2.")
+})
+    
     output$firstDistPlot <- renderPlot({
         
         
@@ -164,34 +178,58 @@ shinyServer(function(input, output) {
     })  
     
     output$thirdTextOutput <- renderText({
-        paste("In the following tables, we can see the differences between gas prices in different cities. With these tables, we can 
-              compare each city to see the difference between the lowest and highest price, and to see all the lowest and
-              highest points as well. From these tables, we can conclude that Los Angeles has the highest difference between 
-              the lowest and highest gas price values standing at a 3.47$ difference. It is also important to notice that 
-              San Francisco is following close behind at 3.21$. When considering the data of the lowest prices recorded 
-              per city, it is important to take into consideration that Boston, Cleveland, Miami and Seattle only started
-              recording their data into this data set from June 2003, while Chicago, Denver, Houston, Los Angeles, New York
-              and San Francisco started recording their data from June 2000. We can see that most cites have their highest 
-              value recorded in July 2008, probably due to the 2008 financial crisis (see the following for more reference: 
-              https://www.investopedia.com/ask/answers/052715/how-did-financial-crisis-affect-oil-and-gas-sector.asp). 
-              Miami and Chicago are anomalies in this context, with their highest values being recorded in a 
-              different year (2017 and 2011, respectively).")
+        paste("In the following tables, we can see the differences between gas prices
+        in different cities. With these tables, we can compare each city
+        to see the difference between the lowest and highest price, and
+        to see all the lowest and highest points as well. From these tables,
+        we can conclude that Los Angeles has the highest difference between 
+        the lowest and highest gas price values standing at a 3.47$ difference.
+        It is also important to notice that San Francisco is following close
+        behind at 3.21$. When considering the data of the lowest prices
+        recorded per city, it is important to take into consideration that
+        Boston, Cleveland, Miami and Seattle only started recording their
+        data into this data set from June 2003, while Chicago, Denver, Houston,
+        Los Angeles, New York and San Francisco started recording their
+        data from June 2000. We can see that most cites have their highest 
+        value recorded in July 2008, probably due to the 2008 financial
+        crisis (see the following for more reference: https://www.investopedia.com/ask/answers/052715/how-did-financial-crisis-affect-oil-and-gas-sector.asp). 
+        Miami and Chicago are anomalies in this context, with their highest
+        values being recorded in a different year (2017 and 2011, respectively).
+        ")
     }) 
     
-    output$introTextOutput <- renderText({
-        paste("In this app, you will be able to review the data of average gas prices in main cities in the US. The main dataset our 
-              app utilizes is from the US Energy Information Administration. See the link if needed: 
-              https://www.eia.gov/dnav/pet/pet_pri_gnd_a_epm0_pte_dpgal_m.htm. Our target audience is consumers of driving age 
-              located in urban areas in the US. Using our app, you will be able to see how gas prices have changed in 
-              urban areas in the US over time, when each city's gas prices were at their highest and lowest prices, which cities 
-              have experienced the highest rates of increases in gas prices, and whether cities' gas prices usually fluctuate
-              together or independently. The main libraries we used to make this app were shiny, tidyverse and ggplot2.")
-    }) 
+    # output$introTextOutput <- renderText({
+    #     paste("In this app, you will be able to review the data of average gas prices in main cities in the US. The main dataset our 
+    #           app utilizes is from the US Energy Information Administration. See the link if needed: 
+    #           https://www.eia.gov/dnav/pet/pet_pri_gnd_a_epm0_pte_dpgal_m.htm. Our target audience is consumers of driving age 
+    #           located in urban areas in the US. Using our app, you will be able to see how gas prices have changed in 
+    #           urban areas in the US over time, when each city's gas prices were at their highest and lowest prices, which cities 
+    #           have experienced the highest rates of increases in gas prices, and whether cities' gas prices usually fluctuate
+    #           together or independently. The main libraries we used to make this app were shiny, tidyverse and ggplot2.")
+    # }) 
     
     output$concTextOutput <- renderText({
-        paste("From the information collected in our app, we can conclude some very important information. We can see
-              that most cities in the US have a fairly linear increase in gas prices over the years, and that the 2008
-              financial crisis had a very big effect on gas prices.")
+        paste("From the information collected in our app, we can conclude some very
+        important information. We can see that most cities in the US have a fairly
+        linear increase in gas prices over the years, and that the 2008 financial
+        crisis resulted in gas prices rising substantially in many US urban areas.
+        Included above are two tables, the first of which shows when the highest
+        gas prices were recorded in each city, and the second showing when each
+        city recorded their lowest gas prices. These tables both draw support to
+        the conclusion made earlier that gas prices across American cities fluctuate
+        together. For instance, it can be seen that six cities recorded their highest
+        gas prices in July of 2008, while four cities recorded their lowest gas
+        prices in December of 2001. Our group feels that we selected a very strong, reliable, and comprehensive
+        dataset. Because the data recorded in this dataset was strictly numerical,
+        and with its source being the US Energy Information Administration, we believe
+        that our dataset provides relatively unbiased results. The numbers provided
+        in the data averaged all prices for all grades of gasoline, so our group
+        feels that the data couldn't have harmed or underrepresented other population
+        groups of varying socioeconomic/financial status. We believe that it would
+        be very worthwhile to continually update this project in the future. As
+        time progresses, gas prices will continue to fluctuate and be tracked, which
+        will allow for other researchers to explore these trends and draw new conclusions
+        from these data.")
     }) 
     
 }
